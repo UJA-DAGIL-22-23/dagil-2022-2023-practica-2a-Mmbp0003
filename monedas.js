40740ee19b3b5396595cb52de725b47272a49de8
Magdalena Bueno Pedrera <mmbp0003@red.ujaen.es>
2023-02-25 00:40:25 +0100

    Refactorizacion

--- monedas.js
+++ monedas.js
@@ -54,68 +54,34 @@ monedas.esMonedaValida = function (valor) {
  */
 monedas.suficienteParaPagar = function (vectorMonedas, montante) {
        // Escribir el código necesario para que vayan pasando las pruebas una a una.
-       
-       if (montante < 0 ){return -2};
-
-       if (vectorMonedas == null && montante == 0){return 1};
-       if ( montante > 0 && vectorMonedas == null) {return 0};
-       if (vectorMonedas.length == 0 && montante == 0){return 1};
-       if (vectorMonedas.length == 0 && montante > 0 ){return 0};
-
-       if (montante >= 1){ 
-         if (vectorMonedas < 0){return -1;} }
-       //Decidi dejarlo en modo texto, porque, de la otra manera funcionaba mejor     
-       
-       if(montante >= 1){
-         existe = new Boolean(true);
-        let suma = 0;
-        for (var i = 0; i < vectorMonedas.length ; i++){
-                if (monedas.esMonedaValida(vectorMonedas[i]) == false){
-                    return -1;
-                }
-            }
-        }
-        
-        
-       if(montante > 0){
+    if (vectorMonedas == null && montante == 0){return 1};
+    if ( montante > 0 && vectorMonedas == null) {return 0};
+    if (vectorMonedas.length == 0 && montante == 0){return 1};
+    if (vectorMonedas.length == 0 && montante > 0 ){return 0};    
+            
+    if(montante > 0){
+        if (vectorMonedas < 0){return -1;} else {
         let suma = 0;
         for (let i = 0; i < vectorMonedas.length ; i++){
             if (monedas.esMonedaValida(vectorMonedas[i]) == true){
                 suma  += vectorMonedas[i];
-            } 
+            }  else if (monedas.esMonedaValida(vectorMonedas[i]) == false){
+                return -1;
+            }
         }
         if ( montante >  suma){
-          return 0;
-        }
+        return 0;
+        } else if (montante == suma){
+                    return 1;
+                } else if (montante < suma){
+                    return 2;
+                }
         }
        
-        
-       if (montante > 0){
-        let suma_2 = 0;
-        for (let i = 0; i < vectorMonedas.length; i++){
-            if (monedas.esMonedaValida(vectorMonedas[i]) == true){
-                suma_2 = suma_2 + vectorMonedas[i];              
-            } 
-        }
-        if (montante == suma_2){
-            return 1;
-        }
-       }
-       
-       if (montante > 0){
-        let suma_3 = 0;
-        for (let i = 0; i < vectorMonedas.length; i++){
-            if (monedas.esMonedaValida(vectorMonedas[i]) == true){
-                suma_3 = suma_3 + vectorMonedas[i];              
-            } 
-        }
-        if (suma_3 > montante){
-            return 2;
-        }
-       }
-    
-
-}     
+    }  else {
+        return -2;
+    }
+}    
 
 /*
 * ----------------------------------------
